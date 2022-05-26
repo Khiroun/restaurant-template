121812c023d05df8bb1af39ef8409e9e667d92c0
Khiroun <em_khiroun@esi.dz>
2022-05-26 18:53:56 +0100

    Use Image component in the subheading

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
-import { images } from "../../constants";
+import Image from "next/image";
 
 const SubHeading = ({ title }) => (
   <div style={{ marginBottom: "1rem" }}>
     <p className="p__cormorant">{title}</p>
-    <img src="/images/spoon.svg" alt="spoon_image" className="spoon__img" />
+    <Image
+      src="/images/spoon.svg"
+      alt="spoon_image"
+      className="spoon__img"
+      width={45}
+      height={45}
+    />
   </div>
 );
 
