64a3f0e19bad5b8e8eb4e41824c941d4dd69fdda
Khiroun <em_khiroun@esi.dz>
2022-05-31 23:17:27 +0100

    Remove robotto font import

--- pages/_document.js
+++ pages/_document.js
@@ -13,10 +13,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="/static/favicon.ico" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {this.props.emotionStyleTags}
         </Head>
